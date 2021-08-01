f16ce384b12f67b477e0470b330f6075a81032a9
Vaibhav Parab <56741689+vaibhav1807@users.noreply.github.com>
2021-08-01 11:37:12 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ console.log('Client side javascript loaded!')
      const location = search.value
 
      messageOne.textContent = 'Fetching weather for location...'
-     fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+     fetch('/weather?address=' + location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express configs
 const publicDir = path.join(__dirname, '../public')
@@ -105,6 +106,6 @@ app.get('*', (req, res)=>{
 
 
 //port
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000.')
+app.listen(port,()=>{
+    console.log('Server is up on port ' +port+ '.')
 })
